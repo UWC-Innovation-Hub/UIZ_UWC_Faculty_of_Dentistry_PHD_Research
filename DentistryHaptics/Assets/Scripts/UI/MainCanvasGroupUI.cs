344471c89322b4cbf68a75bc5737d726504e64ba
Yazeed Williams <yazeedwilliams13@gmail.com>
2025-06-11 14:31:18 +0200

    Removed SO logic
    The SO logic seemed a bit of overengineering for a simple UI

--- DentistryHaptics/Assets/Scripts/UI/MainCanvasGroupUI.cs
+++ DentistryHaptics/Assets/Scripts/UI/MainCanvasGroupUI.cs
@@ -2,24 +2,5 @@ using UnityEngine;
 
 public class MainCanvasGroupUI : MonoBehaviour
 {
-    [Header("Event Channel")]
-    [SerializeField] private HomeSceneUIEventChannelSO homeSceneUIEventChannelSO;
 
-    private void OnEnable()
-    {
-        homeSceneUIEventChannelSO.OnLoginEventRaised += OnLoginEventReceived;
-    }
-
-    private void OnDisable()
-    {
-        homeSceneUIEventChannelSO.OnLoginEventRaised -= OnLoginEventReceived;
-    }
-
-    private void OnLoginEventReceived(GameObject obj)
-    {
-        foreach (Transform child in transform)
-        {
-            child.gameObject.SetActive(true);
-        }
-    }
 }
--- DentistryHaptics/Assets/Scripts/UI/UIController.cs
+++ DentistryHaptics/Assets/Scripts/UI/UIController.cs
@@ -2,21 +2,5 @@ using UnityEngine;
 
 public class UIController : MonoBehaviour
 {
-    [Header("Event Channel")]
-    [SerializeField] private HomeSceneUIEventChannelSO homeSceneUIEventChannelSO;
 
-    private void OnEnable()
-    {
-        homeSceneUIEventChannelSO.OnLoginEventRaised += OnLoginEventReceived;
-    }
-
-    private void OnDisable()
-    {
-        homeSceneUIEventChannelSO.OnLoginEventRaised -= OnLoginEventReceived;
-    }
-
-    private void OnLoginEventReceived(GameObject loginUIGameObject)
-    {
-        loginUIGameObject.SetActive(false);
-    }
 }
